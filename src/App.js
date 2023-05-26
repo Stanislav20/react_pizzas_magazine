4f8583e0a444ce5d7262321c2f3cf472b6795547
Stanislav20 <stas_on38@mail.ru>
2023-05-26 21:02:31 +0800

    Получение данных пицц с бекэнд с помощью сервиса mockapi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './scss/app.scss';
+import { useState, useEffect } from 'react'
+import './scss/app.scss'
 import Header from './components/Header'
 import Categories from './components/Categories'
 import Sort from './components/Sort'
-import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json'
+import PizzaBlock from './components/PizzaBlock'
 
 function App() {
+	const [itemsPizzas, setItemsPizzas] = useState([])
+
+	useEffect(() => {
+		fetch('https://646cb6e27b42c06c3b2bdaff.mockapi.io/items_pizza')
+		.then(res => res.json())
+		.then(arr => setItemsPizzas(arr))
+	}, [])
 
 	return (
 		<div className="wrapper">
@@ -19,7 +26,7 @@ function App() {
 					<h2 className="content__title">Все пиццы</h2>
 					<div className="content__items">
 						{
-								pizzas.map((obj) => (
+								itemsPizzas.map((obj) => (
 									<PizzaBlock 
 										key={obj.id}
 										{...obj}
