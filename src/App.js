29ca2f50fa0951bfa04a0362f3ca7a0ea418a513
Stanislav20 <stas_on38@mail.ru>
2023-05-26 22:46:10 +0800

    Добавлены фейковые (серые, скелетоны) пиццы, которые отображааются на время загрузки данных о пиццах с бекэнда.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './scss/app.scss'
 import Header from './components/Header'
 import Categories from './components/Categories'
 import Sort from './components/Sort'
-import PizzaBlock from './components/PizzaBlock'
+import PizzaBlock from './components/pizzaBlock/index'
+import PizzaSkeleton from './components/pizzaBlock/PizzaSkeleton'
 
 function App() {
 	const [itemsPizzas, setItemsPizzas] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
 		fetch('https://646cb6e27b42c06c3b2bdaff.mockapi.io/items_pizza')
 		.then(res => res.json())
-		.then(arr => setItemsPizzas(arr))
+		.then(arr => {
+			setItemsPizzas(arr)
+			setIsLoading(false)
+		})
 	}, [])
 
 	return (
@@ -26,12 +31,9 @@ function App() {
 					<h2 className="content__title">Все пиццы</h2>
 					<div className="content__items">
 						{
-								itemsPizzas.map((obj) => (
-									<PizzaBlock 
-										key={obj.id}
-										{...obj}
-									/>
-								))
+							isLoading 
+								? [...new Array(6)].map((_, i) => <PizzaSkeleton key={i} />)
+								: (itemsPizzas.map((obj) => (<PizzaBlock key={obj.id} {...obj} />)))
 						}                
 					</div>
 				</div>
