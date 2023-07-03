d5a4bf64ee78b11836f94e16f3d07b34e65f0a91
Stanislav20 <stas_on38@mail.ru>
2023-07-03 20:40:03 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
 	const [searchValue, setSearchValue] = useState('')
 
 	return (
+	
 		<div className="wrapper">
 			<SearchContext.Provider value={{ searchValue, setSearchValue }} >
 				<Header />
