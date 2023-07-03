e81dad01d96219615449ece9b4fdc3b022ede662
Stanislav20 <stas_on38@mail.ru>
2023-07-03 20:58:51 +0800

    BrowserRouter => HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux' /* Подружим приложение и Redux с помощью библиотеки react-redux */
 import './index.css';
 import App from './App';
@@ -9,10 +9,13 @@ import {store} from './redux/store'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	
-	<BrowserRouter>
+	// <BrowserRouter>
+	<HashRouter>
 		<Provider store={store}> 
 			<App />
 		</Provider>
-	</BrowserRouter>
+	</HashRouter>
+
+	// </BrowserRouter>
 	
 );
