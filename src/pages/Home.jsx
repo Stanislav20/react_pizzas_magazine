eb2f919c7af1c92cb485c06dd000c79c89a8f32b
Elizaveta Kriazhkova <elizaveta.kriazhkova@optimax.dev>
2024-08-02 18:20:58 +0400

    added comments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,28 @@
 import { useEffect, useContext } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// useSelector вытаскивает данные из хранилища
-// useDispatch вполняет действия, в данном случает меняет категорию пицц и меняет сортировку пиицц
 
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/pizzaBlock/index';
-import PizzaSkeleton from '../components/pizzaBlock/PizzaSkeleton';
 import { SearchContext } from '../App';
 import { setActiveIndexCategory, setSelectedSort } from '../redux/slices/filterSlice';
 import { fetchPizzas } from '../redux/slices/pizzasSlice';
+import {getActiveIndexCategory, getSelectedSort} from "../redux/selectors/filterSelectors";
+import {PizzaLoader} from "../components/pizzaLoader";
 
 function Home() {
 	const dispatch = useDispatch()
 	const { searchValue } = useContext(SearchContext)
 	const { items, status } = useSelector((state) => state.pizzasReducer)
+	const pizzas = items
+		.filter(obj => obj.title.toLowerCase().includes(searchValue.toLowerCase()))
+		.map((obj) => (<PizzaBlock key={obj.id} {...obj} />))
+	const skeletons = [...new Array(6)].map((_, i) => <PizzaLoader key={i} />)
+	const activeIndexCategory = useSelector(getActiveIndexCategory);
+	const selectedSort = useSelector(getSelectedSort);
 
-	const getPizzas = async () => {
+	// можно вынести в отдельный файл и импортировать
+	const getPizzas = () => {
 		const sortBy = selectedSort.sortProperty.replace('-', '');
 		const order = selectedSort.sortProperty.includes('-') ? 'asc' : 'desc';
 		const category = activeIndexCategory > 0 ? `category=${activeIndexCategory}` : '';
@@ -29,27 +35,17 @@ function Home() {
 		window.scroll(0, 0)
 	}
 
-	const activeIndexCategory = useSelector((state) => state.filterReducer.activeIndexCategory)
-	const dispatchFilterPizzas = useDispatch()
 	const onClickCategory = (index) => {
-		dispatchFilterPizzas(setActiveIndexCategory(index))
+		dispatch(setActiveIndexCategory(index))
 	}
 
-	const selectedSort = useSelector((state) => state.filterReducer.selectedSort)
-	const dispatchSortPizzas = useDispatch()
 	const onClickSort = (obj) => {
-		dispatchSortPizzas(setSelectedSort(obj))
+		dispatch(setSelectedSort(obj))
 	}
 
 	useEffect(() => {
 		getPizzas()
 	}, [activeIndexCategory, selectedSort])
-  
-		const pizzas = items
-		.filter(obj => obj.title.toLowerCase().includes(searchValue.toLowerCase()))
-		.map((obj) => (<PizzaBlock key={obj.id} {...obj} />))	
-
-	const skeletons = [...new Array(6)].map((_, i) => <PizzaSkeleton key={i} />)
 
 	return (
 		<div className="container">
